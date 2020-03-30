0aca1b344107a053c337214bd611ee5a793e9566
trishogen <patriciaogen@gmail.com>
2020-03-30 14:52:46 -0400

    remove test code

--- matching-game-backend/app/controllers/application_controller.rb
+++ matching-game-backend/app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::API
-  def test
-    render json: { test: "success" }
-  end
 end
--- matching-game-backend/config/routes.rb
+++ matching-game-backend/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/test', to: 'application#test'
-
+  
   post '/login', to: 'sessions#create'
 
 end

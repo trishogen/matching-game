071a84b7dcfc9e1425572005dc306301ca02faf8
trishogen <patriciaogen@gmail.com>
2020-04-17 20:52:46 -0400

    refactor signin to its own class

--- matching-game-frontend/src/index.js
+++ matching-game-frontend/src/index.js
@@ -5,62 +5,11 @@ const HEADERS = {
   "Accept": "application/json"
 }
 const IMG_DIR = "../matching-game-backend/app/assets/images"
+let signInForm = undefined;
 
 document.addEventListener("DOMContentLoaded", function(){
-  loadSignInForm();
+  signInForm = new SignInForm(MAIN);
 })
-
-function loadSignInForm(){
-  let div = document.createElement('div');
-  div.className = 'sign-in';
-
-  let form = document.createElement('form');
-  form.id = 'username-form';
-
-  let input = document.createElement('input');
-  input.id = 'username-input';
-  input.type = 'text';
-  input.placeholder = 'username';
-
-  let br = document.createElement("br");
-  br.className = 'br-big'
-
-  let btn = document.createElement('button');
-  btn.type = 'submit';
-  btn.innerText = 'Play!'
-  btn.addEventListener('click', (e) => {
-    signIn(e);
-  });
-
-  form.append(input, br, btn);
-  div.append(form);
-  MAIN.append(div);
-}
-
-function signIn(e){
-  e.preventDefault();
-  let username = document.getElementById('username-input').value;
-
-  let signInObj = {
-    method: "POST",
-    headers: HEADERS,
-    body: JSON.stringify({
-      "username": username
-    })
-  };
-
-  return fetch(BASE_URL + '/login', signInObj)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(object) {
-      return startNewGame(object.id)
-    })
-    .catch(function(error) {
-      alert("I'm having trouble finding that user!");
-      console.log(error.message);
-    });
-}
 
 function startNewGame(user_id){
   let userObj = {
@@ -84,7 +33,7 @@ function startNewGame(user_id){
 }
 
 function loadGame(game){
-  hideSignIn()
+  signInForm.hide()
   addTimer()
 
   let div = document.createElement('div');
@@ -104,11 +53,6 @@ function addTimer(){
   MAIN.append(timer_div)
 
   new Timer('timer')
-}
-
-function hideSignIn(){
-  let signInDiv = document.getElementsByClassName('sign-in')[0];
-  signInDiv.style.display = 'none';
 }
 
 
---
+++ matching-game-frontend/src/signin.js
@@ -0,0 +1,78 @@
+class SignInForm {
+
+  constructor(target) {
+    target.append(this.render());
+
+  }
+
+  render() {
+    let div = document.createElement('div');
+    div.className = 'sign-in';
+
+    let form = this.form()
+    let input = this.usernameInput();
+
+    let br = document.createElement("br");
+    br.className = 'br-big'
+
+    let btn = this.playButton();
+
+    form.append(input, br, btn);
+    div.append(form);
+    return div
+  }
+
+  form() {
+    let form = document.createElement('form');
+    form.id = 'username-form';
+    return form
+  }
+
+  usernameInput(){
+    let input = document.createElement('input');
+    input.id = 'username-input';
+    input.type = 'text';
+    input.placeholder = 'username';
+    return input
+  }
+
+  playButton(){
+    let btn = document.createElement('button');
+    btn.type = 'submit';
+    btn.innerText = 'Play!'
+    btn.addEventListener('click', (e) => {
+      this.signIn(e);
+    });
+    return btn
+  }
+
+  signIn(e) {
+    e.preventDefault();
+    let username = document.getElementById('username-input').value;
+
+    let signInObj = {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify({
+        "username": username
+      })
+    };
+
+    return fetch(BASE_URL + '/login', signInObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        return startNewGame(object.id)
+      })
+      .catch(function(error) {
+        alert("I'm having trouble finding that user!");
+        console.log(error.message);
+      });
+  }
+
+  hide() {
+    let signInDiv = document.getElementsByClassName('sign-in')[0];
+    signInDiv.style.display = 'none';
+  }
+}
